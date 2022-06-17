48a55b65145803b5a937a652613072ca3a0379f3
grimvx <ggfrostz@gmail.com>
2022-06-17 15:42:34 -0700

    Weather and Location

--- config.js
+++ config.js
@@ -27,14 +27,14 @@ const CONFIG = {
 	bentoLayout: 'bento', // 'bento', 'lists', 'buttons'
 
 	// Weather
-	weatherKey: 'InsertYourAPIKeyHere123456', // Write here your API Key
+	weatherKey: '673f5772e70040831b2023fe2d3e98e4', // Write here your API Key
 	weatherIcons: 'OneDark', // 'Onedark', 'Nord', 'Dark', 'White'
-	weatherUnit: 'C', // 'F', 'C'
+	weatherUnit: 'F', // 'F', 'C'
 	language: 'en', // More languages in https://openweathermap.org/current#multi
 
-	trackLocation: true, // If false or an error occurs, the app will use the lat/lon below
-	defaultLatitude: '37.775',
-	defaultLongitude: '-122.419',
+	trackLocation: false, // If false or an error occurs, the app will use the lat/lon below
+	defaultLatitude: '33.564380',
+	defaultLongitude: '-112.250220',
 
 	// Autochange
 	autoChangeTheme: false,
